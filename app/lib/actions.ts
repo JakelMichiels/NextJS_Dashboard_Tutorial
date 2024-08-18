7ea122f676bb4ca517aa62dc5bfe5dd98fb31306
Jake Logan Michiels <jakelmichiels@gmail.com>
2024-08-17 21:04:37 -0400

    fixed redirect bug

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -28,6 +28,6 @@ export async function createInvoice(formData: FormData) {
     INSERT INTO invoices (customer_id, amount, status, date)
     VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
     `;
-    revalidatePath('dashboard/invoices');
-    redirect('dashboard/invoices');
+    revalidatePath('/dashboard/invoices');
+    redirect('/dashboard/invoices');
 }
